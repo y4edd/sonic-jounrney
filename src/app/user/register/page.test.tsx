f2e0ded34f1a08fd3a374e7dddbdcabec761470b
y4edd <yaeyuu0116@icloud.com>
2024-12-04 11:39:48 +0900

    help: ユーザー登録ページの単体テストエラー

--- src/app/user/register/page.test.tsx
+++ src/app/user/register/page.test.tsx
@@ -9,10 +9,20 @@ jest.mock("next/navigation", () => ({
   }),
 }));
 
-// userRegister、onSubmitをモック化
-jest.mock("../../../hooks/useRegister", () => ({
+// userRegister、onSubmit、serverErrorをモック化
+const mockOnRegister = jest.fn((data) => {
+  return Promise.resolve({ success: true });
+});
+
+const mockOnSubmit = jest.fn((data) => {
+  mockOnRegister(data);
+});
+
+jest.mock("@/hooks/useRegister", () => ({
   userRegister: jest.fn(() => ({
-    onSubmit: jest.fn(),
+    RegisterUser: mockOnRegister,
+    onSubmit: mockOnSubmit,
+    serverError: "",
   })),
 }));
 
@@ -52,5 +62,37 @@ describe("UserRegistrationコンポーネントのテスト", () => {
       const errorMessages = screen.getAllByText("パスワードは6文字以上で入力してください");
       expect(errorMessages.length).toBeGreaterThan(0);
     });
+  });
+
+  test("必須フィールドに正常に入力が行われ、ボタンをクリックするとデータが送信されresponseが返ってくる", async () => {
+    const { submitButton } = setUp();
+
+    // 入力箇所の取得
+    const userNameInput = screen.getByLabelText("ユーザー名");
+    const emailInput = screen.getByLabelText("メールアドレス");
+    const passwordInput = screen.getByLabelText("パスワード");
+    const passwordConfirmInput = screen.getByLabelText("パスワード確認");
+
+    // 入力
+    fireEvent.change(userNameInput, { target: { value: "testMan" } });
+    fireEvent.change(emailInput, { target: { value: "testMan@test.com" } });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(passwordConfirmInput, { target: { value: "password" } });
+
+    // ボタンクリック
+    fireEvent.click(submitButton);
+
+    // RegisterUser の呼び出し確認
+    await waitFor(() => {
+      expect(mockOnRegister).toHaveBeenCalledTimes(1);
+      expect(mockOnRegister).toHaveBeenCalledWith({
+        name: "testMan",
+        email: "testMan@test.com",
+        password: "password",
+      });
+    });
+
+    // 呼び出し履歴の詳細をログに出力
+    console.log(mockOnRegister.mock.calls);
   });
 });
